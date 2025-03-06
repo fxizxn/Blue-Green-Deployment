de1a57c11f0514d661641550e3ebedb119811d60
Faizan Quazi <146623765+fxizxn@users.noreply.github.com>
2025-03-06 17:11:29 +0530

    Update main.tf
    updated

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -97,24 +97,27 @@ resource "aws_eks_cluster" "devopsshack" {
 }
 
 resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
+  cluster_name = aws_eks_cluster.devopsshack.name
   node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
+  node_role_arn = aws_iam_role.devopsshack_node_group_role.arn
+
+  subnet_ids = [
+    aws_subnet.devopsshack_subnet[0].id,
+    aws_subnet.devopsshack_subnet[1].id
+  ]
+
+  remote_access {
+    ec2_ssh_key = "DevOps-Shack"
+    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+  }
 
   scaling_config {
     desired_size = 3
     max_size     = 3
     min_size     = 3
   }
-
-  instance_types = ["t2.large"]
-
-  remote_access {
-    ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
-  }
 }
+
 
 resource "aws_iam_role" "devopsshack_cluster_role" {
   name = "devopsshack-cluster-role"
